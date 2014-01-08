3b7769c13bab6374111697df599e808d0a3f20b6
DrRobotmck <mckenneth.scott@icloud.com>
2014-01-08 13:38:23 -0500

    personal portfolio

---
+++ js/buttons.js
@@ -0,0 +1,14 @@
+function buttons(){
+	$(".arrow1").click(function(){
+		$(".main").moveDown();
+	});
+	$(".arrow2").click(function(){
+		$(".main").moveDown();
+	});
+	$(".arrow3").click(function(){
+		$(".main").moveUp();
+	});
+	$(".arrow4").click(function(){
+		$(".main").moveUp();
+	});
+}
